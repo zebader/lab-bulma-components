337a6a42040456dcc73c5b910c2cfcd7e18535c8
Jesus Cebader <zebader@hotmail.com>
2019-05-13 22:27:57 +0200

    first commit

---
+++ lab-bulma-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Navbar.js'
+import NavBar from './Navbar.js';
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
