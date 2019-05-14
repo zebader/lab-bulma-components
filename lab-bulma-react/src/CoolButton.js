b0cb8bebe11d6b4194fbb60618ba700cf3768732
Jesus Cebader <zebader@hotmail.com>
2019-05-14 10:03:04 +0200

    tip with class in button

--- lab-bulma-react/src/CoolButton.js
+++ lab-bulma-react/src/CoolButton.js
@@ -8,3 +8,28 @@ class CoolButton extends React.Component {
   }
 
   export default CoolButton;
+
+/*
+Como hacer cuando los props estas en el jsx 
+<CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+
+import React from 'react';
+
+const createClassString = (props) => {
+  let result = '';
+  if (props.className) result += `${props.className} `; 
+  if (props.isDanger) result += 'is-danger ';
+  if (props.isPrimary) result += 'is-primary ';
+  if (props.isSuccess) result += 'is-success ';
+  return result;
+}
+
+
+
+function CoolButton (props) {
+  return (
+  <button className={`button ${createClassString(props)}`}>{props.children}</button>
+  );
+}
+
+export default CoolButton; */
